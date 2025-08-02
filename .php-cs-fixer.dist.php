ec1413d8c8bc94f6a0b4272dcf6adb40dd17040b
Arthur Dick <github@arthurdick.com>
2025-08-02 03:19:17 -0600

    php-cs-fixer config

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/bin',
+    ])
+    ->append([
+        __FILE__, // Include this file itself
+        __DIR__ . '/build.php',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setFinder($finder)
+;
