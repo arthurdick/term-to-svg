8fe34e3014923a544eb826eae63d98195dd6c32a
Arthur Dick <github@arthurdick.com>
2025-08-07 14:27:58 -0600

    v4.3.0

--- src/TerminalToSvgConverter.php
+++ src/TerminalToSvgConverter.php
@@ -16,7 +16,7 @@ namespace ArthurDick\TermToSvg;
 class TerminalToSvgConverter
 {
     /** @var string The current version of the tool. */
-    public const VERSION = '4.2.1';
+    public const VERSION = '4.3.0';
 
     /** @var resource|false The file handle for the typescript recording. */
     private $typescriptHandle;
